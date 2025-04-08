99fb98aee3afe413a4434e8984578738c9370733
dzodze00 <pakpablie@gmail.com>
2025-04-08 14:51:26 -0700

    Create select.tsx

---
+++ components/ui/select.tsx
@@ -0,0 +1,45 @@
+"use client"
+
+import * as React from "react"
+import { cn } from "@/lib/utils"
+import { ChevronDown } from "lucide-react"
+
+interface SelectProps extends Omit<React.SelectHTMLAttributes<HTMLSelectElement>, "onChange"> {
+  onValueChange?: (value: string) => void
+}
+
+const Select = React.forwardRef<HTMLSelectElement, SelectProps>(
+  ({ className, children, onValueChange, onChange, ...props }, ref) => {
+    const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+      onValueChange?.(event.target.value)
+      if (onChange) {
+        onChange(event)
+      }
+    }
+
+    return (
+      <div className="relative">
+        <select
+          ref={ref}
+          className={cn(
+            "flex h-10 w-full appearance-none rounded-md border border-input bg-background px-3 py-2 pr-8 text-sm ring-offset-background focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+            className,
+          )}
+          onChange={handleChange}
+          {...props}
+        >
+          {children}
+        </select>
+        <ChevronDown className="absolute right-3 top-1/2 h-4 w-4 -translate-y-1/2 opacity-50" />
+      </div>
+    )
+  },
+)
+Select.displayName = "Select"
+
+const SelectItem = React.forwardRef<HTMLOptionElement, React.OptionHTMLAttributes<HTMLOptionElement>>(
+  ({ className, ...props }, ref) => <option ref={ref} className={cn("", className)} {...props} />,
+)
+SelectItem.displayName = "SelectItem"
+
+export { Select, SelectItem }
