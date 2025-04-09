607a09f5f2cc7806b00bc623bc210dc2b6fa6a7c
dzodze00 <pakpablie@gmail.com>
2025-04-08 18:00:34 -0700

    Update separator.tsx

--- components/ui/separator.tsx
+++ components/ui/separator.tsx
@@ -1,27 +1,25 @@
-import type * as React from "react"
+import * as React from "react"
 import { cn } from "@/lib/utils"
 
-function Separator({
-  className,
-  orientation = "horizontal",
-  decorative = true,
-  ...props
-}: React.HTMLAttributes<HTMLDivElement> & {
-  orientation?: "horizontal" | "vertical"
-  decorative?: boolean
-}) {
-  return (
-    <div
-      role={decorative ? "none" : "separator"}
-      aria-orientation={decorative ? undefined : orientation}
-      className={cn(
-        "shrink-0 bg-gray-200",
-        orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]",
-        className,
-      )}
-      {...props}
-    />
-  )
-}
+const Separator = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement> & {
+    orientation?: "horizontal" | "vertical"
+    decorative?: boolean
+  }
+>(({ className, orientation = "horizontal", decorative = true, ...props }, ref) => (
+  <div
+    ref={ref}
+    role={decorative ? "none" : "separator"}
+    aria-orientation={decorative ? undefined : orientation}
+    className={cn(
+      "shrink-0 bg-gray-200",
+      orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]",
+      className
+    )}
+    {...props}
+  />
+))
+Separator.displayName = "Separator"
 
 export { Separator }
