39c43910237bb5208a814a5b5416ab72015f6139
dzodze00 <pakpablie@gmail.com>
2025-04-08 22:26:20 -0700

    Update separator.tsx

--- components/ui/separator.tsx
+++ components/ui/separator.tsx
@@ -12,11 +12,7 @@ const Separator = React.forwardRef<
     ref={ref}
     role={decorative ? "none" : "separator"}
     aria-orientation={decorative ? undefined : orientation}
-    className={cn(
-      "shrink-0 bg-gray-200",
-      orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]",
-      className
-    )}
+    className={cn("shrink-0 bg-border", orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]", className)}
     {...props}
   />
 ))
